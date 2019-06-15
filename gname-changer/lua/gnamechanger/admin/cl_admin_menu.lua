1fa5bbe79f266432e2b7ed7b09639d2829bcb14c
Gabyfle <gaby.santamaria@outlook.fr>
2019-06-15 19:32:34 +0200

    Update cl_admin_menu.lua

--- gname-changer/lua/gnamechanger/admin/cl_admin_menu.lua
+++ gname-changer/lua/gnamechanger/admin/cl_admin_menu.lua
@@ -6,11 +6,12 @@
                 Steam : https://steamcommunity.com/id/EpicGaby
 
 -----------------------------------------------------------------------------]]
+local PANEL = {}
 
-local function mainDerma()
+function PANEL:Init()
     local w, h, ply = ScrW(), 300, LocalPlayer()
 
-    local config = net.ReadTable()
+    local config = ply.gNConfig
 
     if w < 480 then
         w = 380
@@ -22,103 +23,100 @@ local function mainDerma()
         w = 600
     end
 
-    local frame = vgui.Create("DFrame")
-          frame:SetSize(w, h)
-          frame:SetTitle("")
-          frame:SetDraggable(false)
-          frame:ShowCloseButton(false)
-          frame:Center()
-          frame:MakePopup()
-          function frame:Paint(w, h)
-                surface.SetDrawColor(gNameChanger.dermaColor.r, gNameChanger.dermaColor.g, gNameChanger.dermaColor.b, 255)
-                surface.DrawRect(0, 0, w, h)
-          end
-    --[[---------------------------------------------------------------------------------
-            Main frame elements
-                title       : Displayed on top left, it's the main title of the frame
-                closeBut    : A little red button to close the main frame, on top right
-                namesLab    : Blacklist textentry label
-                namesText   : blacklist textentry
-                activeCheck : "Active blacklist" checkbox button
-                saveBut     : Settings saving button
-    -----------------------------------------------------------------------------------]]
-    local title = vgui.Create("DLabel", frame)
-          title:SetText("gNameChanger - " .. gNameChanger.Language.administration)
-          title:SetColor(gNameChanger.dermaFontColor)
-          title:SetFont("montserrat-medium")
-          title:SetPos(15, 2)
-          title:SizeToContents()
+    self:SetSize(w, h)
+    self:SetTitle("")
+    self:SetDraggable(false)
+    self:ShowCloseButton(false)
+    self:Center()
+    self:MakePopup()
 
-    local closeBut = vgui.Create("DButton", frame)
-          closeBut:SetText("X")
-          closeBut:SetPos(w - 35, 5)
-          closeBut:SetSize(30, 20)
-          closeBut:SetColor(gNameChanger.dermaFontColor)
-          closeBut:SetFont("roboto-light")
-          function closeBut:Paint(w, h)
+    local title = vgui.Create("DLabel", self)
+        title:SetText("gNameChanger - " .. gNameChanger.Language.administration)
+        title:SetColor(gNameChanger.dermaFontColor)
+        title:SetFont("montserrat-medium")
+        title:SetPos(15, 2)
+        title:SizeToContents()
+
+    local closeBut = vgui.Create("DButton", self)
+        closeBut:SetText("X")
+        closeBut:SetPos(w - 35, 5)
+        closeBut:SetSize(30, 20)
+        closeBut:SetColor(gNameChanger.dermaFontColor)
+        closeBut:SetFont("roboto-light")
+        function closeBut:Paint(w, h)
                 surface.SetDrawColor(Color(230, 57, 70))
                 surface.DrawRect(0, 0, w, h)
-          end
-          closeBut.DoClick = function()
-                frame:Close()
-          end
+        end
+        closeBut.DoClick = function()
+                self:Close()
+        end
+    local namesLab = vgui.Create("DLabel", self)
+        namesLab:SetText(gNameChanger.Language.blacklist .. " :")
+        namesLab:SetFont("roboto-light")
+        namesLab:SetPos(30, 40)
+        namesLab:SizeToContents()
 
-    local namesLab = vgui.Create("DLabel", frame)
-          namesLab:SetText(gNameChanger.Language.blacklist .. " :")
-          namesLab:SetFont("roboto-light")
-          namesLab:SetPos(30, 40)
-          namesLab:SizeToContents()
-
-    local namesText = vgui.Create("DTextEntry", frame)
-          namesText:SetPos(30, 65)
-          namesText:SetSize((w - 60), 100)
-          namesText:SetText(config.names)
-          namesText:SetMultiline(true)
-          namesText:SetFont("roboto-light")
-          namesText:SetDrawLanguageID(false)
-          function namesText.Paint(self)
+    local namesText = vgui.Create("DTextEntry", self)
+        namesText:SetPos(30, 65)
+        namesText:SetSize((w - 60), 100)
+        namesText:SetText(config.names)
+        namesText:SetMultiline(true)
+        namesText:SetFont("roboto-light")
+        namesText:SetDrawLanguageID(false)
+        function namesText.Paint(self)
                 surface.SetDrawColor(gNameChanger.dermaColor.r + 40, gNameChanger.dermaColor.g + 70, gNameChanger.dermaColor.b + 70)
                 surface.DrawRect(0, 0, self:GetWide(), self:GetTall())
                 self:DrawTextEntryText(gNameChanger.dermaFontColor, Color(gNameChanger.dermaColor.r, gNameChanger.dermaColor.g, gNameChanger.dermaColor.b), Color(gNameChanger.dermaColor.r, gNameChanger.dermaColor.g, gNameChanger.dermaColor.b))
-          end
+        end
+    local activeCheck = vgui.Create("DCheckBox", self)
+        activeCheck:SetPos(30, namesText:GetTall() + 85)
+        activeCheck:SetValue(config.active)
+        function activeCheck.Paint(self)
+            surface.SetDrawColor(gNameChanger.dermaColor.r + 40, gNameChanger.dermaColor.g + 70, gNameChanger.dermaColor.b + 70)
+            surface.DrawRect(0, 0, self:GetWide(), self:GetTall())
+            if self:GetChecked() then
+                surface.SetDrawColor(Color(123, 179, 90))
+                surface.DrawRect(self:GetWide() - 12.5, self:GetTall() - 12.5, self:GetWide() - 5, self:GetTall() - 5)
+            end
+        end
+    local activeLabel = vgui.Create("DLabel", self)
+        activeLabel:SetText(gNameChanger.Language.activeList)
+        activeLabel:SetFont("roboto-light")
+        activeLabel:SetPos(50, namesText:GetTall() + 82.5)
+        activeLabel:SetMouseInputEnabled(true)
+        activeLabel:SizeToContents()
+        activeLabel.DoClick = function() activeCheck:Toggle() end
 
-    local activeCheck = vgui.Create("DCheckBox", frame)
-          activeCheck:SetPos(30, namesText:GetTall() + 85)
-          activeCheck:SetValue(config.active)
-          function activeCheck.Paint(self)
-                surface.SetDrawColor(gNameChanger.dermaColor.r + 40, gNameChanger.dermaColor.g + 70, gNameChanger.dermaColor.b + 70)
-                surface.DrawRect(0, 0, self:GetWide(), self:GetTall())
-                if self:GetChecked() then
-                    surface.SetDrawColor(Color(123, 179, 90))
-                    surface.DrawRect(self:GetWide() - 12.5, self:GetTall() - 12.5, self:GetWide() - 5, self:GetTall() - 5)
-                end
-          end
-    local activeLabel = vgui.Create("DLabel", frame)
-          activeLabel:SetText(gNameChanger.Language.activeList)
-          activeLabel:SetFont("roboto-light")
-          activeLabel:SetPos(50, namesText:GetTall() + 82.5)
-          activeLabel:SetMouseInputEnabled(true)
-          activeLabel:SizeToContents()
-          activeLabel.DoClick = function() activeCheck:Toggle() end
-
-    local saveBut = vgui.Create("DButton", frame)
-          saveBut:SetText(gNameChanger.Language.saveConfig)
-          saveBut:SetPos(10, h - 50)
-          saveBut:SetSize(w - 20, 40)
-          saveBut:SetColor(gNameChanger.dermaFontColor)
-          saveBut:SetFont("roboto-light")
-          function saveBut:Paint(w, h)
+    local saveBut = vgui.Create("DButton", self)
+        saveBut:SetText(gNameChanger.Language.saveConfig)
+        saveBut:SetPos(10, h - 50)
+        saveBut:SetSize(w - 20, 40)
+        saveBut:SetColor(gNameChanger.dermaFontColor)
+        saveBut:SetFont("roboto-light")
+        function saveBut:Paint(w, h)
                 surface.SetDrawColor(Color(123, 179, 90))
                 surface.DrawRect(0, 0, w, h)
-          end
-          saveBut.DoClick = function()
+        end
+        saveBut.DoClick = function()
                 local active, names = activeCheck:GetChecked(), namesText:GetText()
                 net.Start("gNameChanger:Admin:Save")
                     net.WriteBool(active)
                     net.WriteString(names)
                 net.SendToServer()
-                frame:Close()
-          end
+                self:Close()
+        end
 end
 
-net.Receive("gNameChanger:Admin:Panel", mainDerma)
+function PANEL:Paint(w, h)
+      surface.SetDrawColor(gNameChanger.dermaColor.r, gNameChanger.dermaColor.g, gNameChanger.dermaColor.b, 255)
+      surface.DrawRect(0, 0, w, h)
+end
+
+vgui.Register("gNameChanger:PANEL:Administration", PANEL, "DFrame")
+
+net.Receive("gNameChanger:Admin:Panel", function(len)
+    local ply = LocalPlayer()
+    ply.gNConfig = net.ReadTable()
+    local adminPanel = vgui.Create("gNameChanger:PANEL:Administration")
+    adminPanel:MakePopup()
+end)
